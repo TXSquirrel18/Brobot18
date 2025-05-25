2b818e022c6abb77163d689c45ae7966d5aa707c
TXSquirrel18 <prestontyler18@yahoo.com>
2025-05-24 19:23:56 -0500

    Update index.js
    Signed-off-by: TXSquirrel18 <prestontyler18@yahoo.com>

--- index.js
+++ index.js
@@ -1,25 +1,11 @@
-app.get('/', (req, res) => {
-  res.send('Brobot18 API is running.');
-});
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.use(express.json());
-
-app.post('/activate-intent-layer', (req, res) => {
-  const { trackEdits, autoSuggest, toneDetection, cacheSize, lockInPrompt } = req.body;
-
-  if (trackEdits === undefined || autoSuggest === undefined || toneDetection === undefined) {
-    return res.status(400).json({ error: 'Missing required fields' });
-  }
-
-  res.json({
-    status: 'activated',
-    config: { trackEdits, autoSuggest, toneDetection, cacheSize, lockInPrompt }
-  });
+app.get('/', (req, res) => {
+  res.send('Brobot API is running');
 });
 
 app.listen(port, () => {
-  console.log(`Brobot API is running`);
+  console.log(`Server running on port ${port}`);
 });
